11134f6fecfece42a7ea9820199d174034ce73e5
ldoublewood <ldoublewood@gmail.com>
2026-02-22 11:32:18 +0800

    fixed long ping issue

--- p2p/transport/quicreuse/config.go
+++ p2p/transport/quicreuse/config.go
@@ -7,10 +7,10 @@ import (
 )
 
 var quicConfig = &quic.Config{
-	MaxIncomingStreams:         256,
-	MaxIncomingUniStreams:      5,              // allow some unidirectional streams, in case we speak WebTransport
-	MaxStreamReceiveWindow:     10 * (1 << 20), // 10 MB
-	MaxConnectionReceiveWindow: 15 * (1 << 20), // 15 MB
+	MaxIncomingStreams:         100000,
+	MaxIncomingUniStreams:      5,               // allow some unidirectional streams, in case we speak WebTransport
+	MaxStreamReceiveWindow:     50 * (1 << 20),  // 10 MB
+	MaxConnectionReceiveWindow: 100 * (1 << 20), // 15 MB
 	KeepAlivePeriod:            15 * time.Second,
 	Versions:                   []quic.Version{quic.Version1},
 	// We don't use datagrams (yet), but this is necessary for WebTransport
